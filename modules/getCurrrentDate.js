9c4df66222b617a809a374ac40a20c992a68f365
ometman <revomet@gmail.com>
2023-05-09 16:35:33 +0000

    Add date from luxon date api

--- modules/getCurrrentDate.js
+++ modules/getCurrrentDate.js
@@ -1,19 +1,17 @@
 /* eslint-disable no-use-before-define */
 // get date from luxon
-import { DateTime } from '../node_modules/luxon/src/luxon.js';
+import { DateTime } from './luxon.js';
 import { mainHeader } from './header.js';
 
-const currentDate = () => {
-  const dt = DateTime.fromObject({
-    year: 2023, month: 5, day: 8, hour: 12,
-  }, { zone: UTC, numberingSystem: Eng });
-  const date = dt.toLocaleString(DateTime.DATE_HUGE);
+export const currentDate = () => {
+  const dt = DateTime.now();
+  const currentDateTime = dt.toLocaleString(DateTime.DATE_HUGE);
 
-  const dateContainer = document.createElement('div').className('current-date');
-  dateContainer.append(document.createElement('p').innerHTML = date);
+  const dateContainer = document.createElement('div');
+  dateContainer.className = 'current-date';
+  dateContainer.append(document.createElement('p').innerHTML = currentDateTime);
   mainHeader.appendChild(dateContainer);
-  
 };
 
-export { currentDate };
+export { currentDate as default };
 
